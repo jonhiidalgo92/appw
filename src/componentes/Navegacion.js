76f1e05345ae701dab985dd21f5d6b6b8b902318
Jose Wannan <2993999310101@ingenieria.usac.edu.gt>
2021-03-22 23:43:14 -0600

    Modificacion Pestañas

--- src/componentes/Navegacion.js
+++ src/componentes/Navegacion.js
@@ -11,37 +11,20 @@ export default class Navegacion extends Component {
             </Link>
             
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
+                <span className="navbar-toggler-icon">#</span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNav">
                
                 <ul className="navbar-nav ml-auto">
 
                     <li className="nav-item">
-                        <Link className="nav-link" to="/data">
-                            Datos Modulos
-                        </Link>
-                    </li>
-
-                    <li className="nav-item">
                        <Link className="nav-link" to="/repo">
-                            Reportes
-                        </Link>
-                    </li>
-
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/top">
-                            Top
+                            System Monitor
                         </Link>
                     </li>
                     <li className="nav-item">
                         <Link className="nav-link" to="/funel">
-                            Funel
-                        </Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/type">
-                           Infectedtype
+                            Mensajeria
                         </Link>
                     </li>
                 </ul>
